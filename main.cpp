b723ecb241608c9738d7f7c5b6cdc61a5b1f3e7c
Anto-kumar <antokumar00@gmail.com>
2026-02-07 01:20:11 +0600

    modified vehicle class

--- main.cpp
+++ main.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 class Vehicle{
 
-    
+protected:
+    int id;
+    string brand;
+    string model;
+    double pricePerDay;
+    bool available;
+    string vehicleType;
+
+public:
+    Vehicle(int i, string b, string m, double price, string type){
+        id = i;
+        brand= b;
+        model = m;
+        pricePerDay =price;
+        vehicleType = type;
+    }
 
 };
